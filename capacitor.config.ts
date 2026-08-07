cc2b30f3a8f16b4c42d30eacb32148c9b7c12179
Sushma <sushmashenoy1997@gmail.com>
2026-08-07 15:02:12 -0700

    Update Capacitor app identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.sushma.starter',
   appName: 'Gigglebean',
   webDir: 'www',
   plugins: {
